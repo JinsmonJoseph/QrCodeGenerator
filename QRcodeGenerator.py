b83c068af462a3fd3432970b6b61c248ceb19a41
Jinsmon Joseph <52479335+JinsmonJoseph@users.noreply.github.com>
2022-10-16 18:42:34 -0700

    Added function to create QR

--- QRcodeGenerator.py
+++ QRcodeGenerator.py
@@ -10,3 +10,15 @@ root.title("QrCodeGenerator")
 root.minsize(width=400,height=400)
 root.geometry("500x500")
 root.mainloop()
+
+#Create QR from text stored in Clipboard
+def createQr():
+    input_data = root.clipboard_get()
+    qr = qrcode.QRCode(
+            version=5,
+            box_size=10,
+            border=5)
+    qr.add_data(input_data)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    img.save('QR.png')
