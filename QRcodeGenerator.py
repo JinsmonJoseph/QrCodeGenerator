aa26c2e599f21812e553f2e318c490df41d0bb06
Jinsmon Joseph <52479335+JinsmonJoseph@users.noreply.github.com>
2022-10-16 18:40:26 -0700

    Created python file

---
+++ QRcodeGenerator.py
@@ -0,0 +1,12 @@
+#imports
+from tkinter import *
+from tkinter import *
+import qrcode
+from PIL import ImageTk,Image 
+
+#creating the window
+root = Tk()
+root.title("QrCodeGenerator")
+root.minsize(width=400,height=400)
+root.geometry("500x500")
+root.mainloop()
