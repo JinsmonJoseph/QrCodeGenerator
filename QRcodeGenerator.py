6c4b0170251d5768c4424a1067c74876b2684bcf
Jinsmon Joseph <52479335+JinsmonJoseph@users.noreply.github.com>
2022-10-16 18:46:32 -0700

    Added code to display QR and Button

--- QRcodeGenerator.py
+++ QRcodeGenerator.py
@@ -22,3 +22,19 @@ def createQr():
     qr.make(fit=True)
     img = qr.make_image(fill='black', back_color='white')
     img.save('QR.png')
+#Function to perform refresh
+def refresh():
+    pass
+#Display QR Code
+createQr()
+QRimg =Image.open("QR.png")
+QRimg = QRimg.resize((350,350),Image.ANTIALIAS)
+img = ImageTk.PhotoImage(QRimg)
+Canvas1 = Canvas(root)
+imageContainer=Canvas1.create_image(250,190,image = img)  
+Canvas1.config(bg="black",width = 300, height = 300)
+Canvas1.pack(expand=True,fill=BOTH)
+
+#Button to perform refresh
+RefreshBtn = Button(root,text="Refresh",bg='green', fg='black',font=('Helvetica',20,"bold"), command=refresh)
+RefreshBtn.place(relx=0.35,rely=0.85, relwidth=0.27,relheight=0.1)
